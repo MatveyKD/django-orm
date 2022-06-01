dd18e6814404075276e5038fdaadad3968dc1d61
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-06-01 11:31:26 +0300

    Избавился от лишней функции

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,26 +6,6 @@ import django
 import datetime
 
 
-def get_duration(visit):
-    now = datetime.datetime.now().astimezone()
-
-    entered_at = django.utils.timezone.localtime(visit.entered_at)
-    if visit.leaved_at:
-        located_time = visit.leaved_at-entered_at
-    else:
-        located_time = now-entered_at
-    seconds = located_time.total_seconds()
-    hours = int(seconds // 3600)
-    minutes = int(seconds % 3600 // 60)
-    time_inside = f"{hours}:{minutes}"
-    return located_time
-
-
-def is_visit_long(visit, minutes=60):
-    duration = get_duration(visit)
-    return duration.total_seconds() > 3600
-
-
 def storage_information_view(request):
     non_closed_visits = []
 
