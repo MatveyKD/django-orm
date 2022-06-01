a4384086de9818823f8637f9e1a004c466cc8c3c
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-06-01 11:25:35 +0300

    Уточнил названия переменных

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -6,22 +6,22 @@ from django.shortcuts import render
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
 
-    this_passcard_visits = []
+    serialized_passcard_visits = []
 
-    visits = Visit.objects.filter(passcard=passcard)
-    for visit in visits:
+    this_visits = Visit.objects.filter(passcard=passcard)
+    for visit in this_visits:
         visit_duration = visit.get_duration()
         hours = visit_duration.total_seconds() // 3600
         minutes = visit_duration.total_seconds() // 60
-        this_passcard_visits.append({
+        serialized_passcard_visits.append({
             'entered_at': visit.entered_at,
             'duration': f'{hours}:{minutes}',
             'is_strange': visit.is_long()
         })
-    print(this_passcard_visits)
+    print(serialized_passcard_visits)
 
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': serialized_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
