b2916763429321f51d6537cf7b04608021f78dd1
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-06-11 10:22:35 +0300

    Изменил ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ DEBUG = "true" in os.getenv('DEBUG').lower()
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
