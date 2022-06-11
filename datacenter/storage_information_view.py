7aa0d23bb872b5201a07b0977802ff7c38078208
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-06-11 10:34:50 +0300

    Уточнил названия переменных

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -7,23 +7,25 @@ import datetime
 
 
 def storage_information_view(request):
-    non_closed_visits = []
+    non_closed_visits_info = []
 
-    visits = Visit.objects.filter(leaved_at=None)
-    for visit in visits:
+    active_passcards = Passcard.objects.filter(is_active=True)
+
+    non_closed_visits = Visit.objects.filter(leaved_at=None)
+    for visit in non_closed_visits:
         located_time = visit.get_duration()
         seconds = located_time.total_seconds()
         hours = int(seconds // 3600)
         minutes = int(seconds % 3600 // 60)
         time_inside = f"{hours}:{minutes}"
         name = visit.passcard.owner_name
-        non_closed_visits.append({
+        non_closed_visits_info.append({
             'who_entered': name,
             'entered_at': visit.entered_at,
             'duration': time_inside
         })
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': non_closed_visits_info,
     }
     return render(request, 'storage_information.html', context)
