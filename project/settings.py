ed4c11d38a0f693c7f9037d82504f7daa75d9fe8
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-06-11 11:18:35 +0300

    Добавил DB_ к названиям переменных окружения

--- project/settings.py
+++ project/settings.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('ENGINE'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
@@ -18,7 +18,7 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = "true" in os.getenv('DEBUG').lower()
+DEBUG = "true" in os.getenv('DB_DEBUG').lower()
 
 ROOT_URLCONF = 'project.urls'
 
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['127.0.0.1:8000']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
-        'BACKEND': os.getenv('BACKEND'),
+        'BACKEND': os.getenv('DB_BACKEND'),
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
     },
@@ -37,9 +37,9 @@ TEMPLATES = [
 
 USE_L10N = True
 
-LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
+LANGUAGE_CODE = os.getenv('DB_LANGUAGE_CODE')
 
-TIME_ZONE = os.getenv('TIME_ZONE')
+TIME_ZONE = os.getenv('DB_TIME_ZONE')
 
 USE_TZ = True
 
